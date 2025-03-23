6f2ad6637e93a800862ec9b846a949c4c3e0688a
FcoGomez10 <fcogomez10@outlook.com>
2025-03-22 19:15:55 -0600

    Funciones Prueba.sql

--- compartirCosas/Funciones Prueba.sql
+++ compartirCosas/Funciones Prueba.sql
@@ -1,1 +1,19 @@
+------------------ Funcion Total a pagar ----------------------------------
+
+/*delimiter //
+drop function fn_get_producto;*/
+
+delimiter //
+create function fn_get_total ( p_precio double, p_numNoches double)
+returns double deterministic 
+begin 
+	declare total double; 
+    
+	set total = p_precio * p_numNoches; 
+	return total;
+end;
+
+select fn_get_total (142, 3) as Total;
+
+---------------------- Funcion Cantidad de Reservaciones -------------------
 
