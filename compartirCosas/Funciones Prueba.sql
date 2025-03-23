cf2fe950a4e20c94a1e205280fc9129900505253
GustavoF <122200758+Tavhoo@users.noreply.github.com>
2025-03-22 19:22:07 -0600

    Update Funciones Prueba.sql

--- compartirCosas/Funciones Prueba.sql
+++ compartirCosas/Funciones Prueba.sql
@@ -17,3 +17,32 @@ select fn_get_total (142, 3) as Total;
 
 ---------------------- Funcion Cantidad de Reservaciones -------------------
 
+
+
+---------------------- Funcion confirmacion de Reservaciones -------------------
+
+delimiter //
+drop function fn_confirmacion;
+
+delimiter //
+create function fn_confirmacion (id_cliente int, id_hotel int)
+returns varchar(200) deterministic
+begin
+	declare cliente varchar(45);
+    declare hotel varchar(45);
+    
+    set cliente = (select concat(nombre, " ", apellido) from cliente
+		where id_cliente= id_cliente);
+        
+	set hotel =(select nombre from hotel where id_hotel=id_hotel limit 1);
+    
+    if hotel is not null then
+		return concat(cliente, " gracias por su reservacion en el hotel ", hotel);
+	
+    else  return concat(cliente, " No tiene hotel");
+    
+    end if;
+        
+end;
+
+select fn_confirmacion(1, 1) as Confirmacion;
